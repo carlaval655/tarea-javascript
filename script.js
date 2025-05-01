7cd012ad50410eccb7c6b2d1c35b3402040260fd
carlaval655 <carla.val655@gmail.com>
2025-04-30 21:34:49 -0400

    Creacion del proyecto

---
+++ script.js
@@ -0,0 +1,13 @@
+// Obtener elementos del DOM
+const verDetallesButton = document.getElementById('verDetalles');
+const volverButton = document.getElementById('volver');
+const tarjeta = document.querySelector('.tarjeta');
+// Función para voltear la tarjeta
+verDetallesButton.addEventListener('click', () => {
+    tarjeta.classList.add('volteada');
+  });
+  
+  // Función para volver a la cara frontal
+  volverButton.addEventListener('click', () => {
+    tarjeta.classList.remove('volteada');
+  });
